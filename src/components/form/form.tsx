cef85603daee30f01eb136d1309bc98261142cbb
HazWahb8080 <hazem.wahb100@gmail.com>
2022-06-13 21:00:01 +0200

    adding logic for the Form

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,17 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Button } from "../button/button";
 import styles from "./form.module.css";
-import { ModalContext } from "./../../context/useContext";
+import {
+  MessageContext,
+  ModalContext,
+  ProductsContext,
+} from "./../../context/useContext";
+import { Product } from "./../../../typings.d";
 
 export const Form = () => {
   const { setModalIsOpen } = useContext<any>(ModalContext);
+  const { setMessage } = useContext<any>(MessageContext);
   const [newProduct, setNewProduct] = useState<Product>({
     title: "",
     description: "",
     price: 0,
     rating: { rate: 0, count: 0 }, // added based on the scope of the project overall. as customers who will rate the product
   });
+  const { products, setProducts } = useContext<any>(ProductsContext);
 
+  useEffect(() => {
+    if (!newProduct.title) return; //because it runs after the first render
+    const postingNewProduct = async () => {
+      await fetch("https://fakestoreapi.com/products", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newProduct),
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          setProducts([newProduct, ...products]); //adding the new product
+          setMessage({
+            isShowing: true,
+            content: "Product Added Successfully!",
+          }); // important for the user to know that the product was added or not ?
+          setTimeout(() => {
+            setMessage({ isShowing: false, content: "" });
+          }, 2000);
+        })
+        .catch((err) => {
+          setMessage({
+            isShowing: true,
+            content: "Error Adding your Product , please try again!",
+          }); // if there is an error adding the product, the message will not disappear.
+        });
+    };
+    postingNewProduct();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newProduct]);
   return (
     <form
       className={styles.form}
